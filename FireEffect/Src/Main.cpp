c136242b13851ff0a0ea39bf8b2527ef503c5624
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-04-26 07:51:52 +0900

    프로젝트 개발을 위한 기본적인 메인 함수 추가

---
+++ FireEffect/Src/Main.cpp
@@ -0,0 +1,35 @@
+#include <cstdint>
+#include <Windows.h>
+
+#include <glad/glad.h>
+#include <imgui.h>
+
+#include "CrashModule.h"
+#include "MathModule.h"
+#include "PlatformModule.h"
+#include "RenderModule.h"
+#include "Shader.h"
+
+int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
+{
+	PlatformModule::WindowConstructParams windowParam { L"FireEffect", 100, 100, 1000, 1000, false, false };
+
+	CrashModule::Init();
+	PlatformModule::Init(windowParam);
+	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
+	RenderModule::Init(PlatformModule::GetWindowHandle());
+
+	RenderModule::SetPointSizeMode(true);
+
+	PlatformModule::RunLoop(
+		[&](float deltaSeconds)
+		{
+			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+			RenderModule::EndFrame();
+		}
+	);
+	
+	PlatformModule::Uninit();
+	CrashModule::Uninit();
+	return 0;
+}
