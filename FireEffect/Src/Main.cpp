2edc58b7a2324fd61435d7244735136826061547
CHOIJIONE\choij <choijione02@gmail.com>
2024-04-26 10:53:19 +0900

    효과를 보여주는 창과 효과를 제어하는 창 추가

--- FireEffect/Src/Main.cpp
+++ FireEffect/Src/Main.cpp
@@ -12,7 +12,7 @@
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
-	PlatformModule::WindowConstructParams windowParam { L"FireEffect", 100, 100, 1000, 1000, false, false };
+	PlatformModule::WindowConstructParams windowParam { L"FireEffect", 100, 100, 1000, 800, false, false };
 
 	CrashModule::Init();
 	PlatformModule::Init(windowParam);
@@ -25,6 +25,20 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
+			ImGui::Begin("Effect", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+			{
+				ImGui::SetWindowPos(ImVec2(0.0f, 0.0f));
+				ImGui::SetWindowSize(ImVec2(700.0f, 800.0f));
+			}
+			ImGui::End();
+
+			ImGui::Begin("Properties", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+			{
+				ImGui::SetWindowPos(ImVec2(700.0f, 0.0f));
+				ImGui::SetWindowSize(ImVec2(300.0f, 800.0f));
+			}
+			ImGui::End();
+
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 			RenderModule::EndFrame();
 		}
