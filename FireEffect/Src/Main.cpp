4bac73a86c96bfacd7ce66b1a118f45e819104a7
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-04-26 16:45:46 +0900

    난수 생성 기능 추가
    - https://greentec.github.io/shadertoy-fire-shader-en/

--- FireEffect/Src/Main.cpp
+++ FireEffect/Src/Main.cpp
@@ -9,6 +9,7 @@
 #include "PlatformModule.h"
 #include "RenderModule.h"
 #include "Shader.h"
+#include "ColorFrameBuffer.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -24,23 +25,14 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 
 	Shader* fireEffect = RenderModule::CreateResource<Shader>("Shader/FireEffect.comp");
 
-	uint32_t width = 700;
-	uint32_t height = 700;
-	uint32_t image[2];
-	glGenTextures(2, image);
-	for (uint32_t index = 0; index < 2; ++index)
-	{
-		glBindTexture(GL_TEXTURE_2D, image[index]);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, nullptr);
-		glTextureParameteri(image[index], GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTextureParameteri(image[index], GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTextureParameteri(image[index], GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-		glTextureParameteri(image[index], GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-		glBindTexture(GL_TEXTURE_2D, 0);
-	}
+	int32_t width = 600;
+	int32_t height = 600;
+	ColorFrameBuffer* colorFrameBuffer = RenderModule::CreateResource<ColorFrameBuffer>(width, height);
 
-	int32_t currentImage = 0;
-
+	float a = 0.0f;
+	float b = 0.0f;
+	float c = 0.0f;
+	
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
@@ -51,7 +43,7 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 				ImVec2 uv0 = ImVec2(0.0f, 1.0f); // 텍스처 좌측 상단
 				ImVec2 uv1 = ImVec2(1.0f, 0.0f); // 텍스처 우측 하단
 
-				ImGui::Image((void*)(intptr_t)(image[currentImage]), ImVec2(600.0f, 600.0f), uv0, uv1);
+				ImGui::Image((void*)(intptr_t)(colorFrameBuffer->GetColorBufferID()), ImVec2(600.0f, 600.0f), uv0, uv1);
 			}
 			ImGui::End();
 
@@ -59,27 +51,32 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 			{
 				ImGui::SetWindowPos(ImVec2(700.0f, 0.0f));
 				ImGui::SetWindowSize(ImVec2(300.0f, 800.0f));
+
+				ImGui::SliderFloat("a", &a, 0.0f, 25.0f);
+				ImGui::SliderFloat("b", &b, 0.0f, 150.0f);
+				ImGui::SliderFloat("c", &c, 0.0f, 100000.0f);
 			}
 			ImGui::End();
+			
+			RenderModule::BeginFrame(1.0f, 1.0f, 1.0f, 1.0f);
 
 			fireEffect->Bind();
 			{
-				glBindImageTexture(0, image[(currentImage + 0) % 2], 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
-				glBindImageTexture(1, image[(currentImage + 1) % 2], 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+				fireEffect->SetUniform("a", a);
+				fireEffect->SetUniform("b", b);
+				fireEffect->SetUniform("c", c);
+
+				colorFrameBuffer->Clear(0.0f, 0.0f, 0.0f, 0.0f);
+				glBindImageTexture(0, colorFrameBuffer->GetColorBufferID(), 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 				glDispatchCompute(width, height, 1);
 				glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 			}
 			fireEffect->Unbind();
 
-			currentImage = (currentImage + 1) % 2;
-
-			RenderModule::BeginFrame(1.0f, 1.0f, 1.0f, 1.0f);
 			RenderModule::EndFrame();
 		}
 	);
-
-	glDeleteTextures(2, image);
-
+	
 	PlatformModule::Uninit();
 	CrashModule::Uninit();
 	return 0;
